860ef120feb098fbde75116c001b12479f6bb385
Konstantin <chachabooms@gmail.com>
2022-01-31 00:02:08 +0300

    add auth

--- R/parse.R
+++ R/parse.R
@@ -1,10 +1,18 @@
 ### const ----
 credentals <- jsonlite::fromJSON(Sys.getenv("credentals"))
+auth_google <- Sys.getenv("GKEY")
+name_google <- credentals[["GNAME"]]
 link <- credentals[["GLINK_PARSE"]]
 targets_parse <- credentals[["GPARSE_TARG"]]
 result_parse <- credentals[["GPARSE_RES"]]
 time_local <- "Europe/Moscow"
 delay <- 0.3
+
+### auth ----
+file_con <- file(name_google)
+writeLines(auth_google, file_con)
+close(file_con)
+googlesheets4::gs4_auth(path = name_google)
 
 ### get links ----
 links_parse <- googlesheets4::read_sheet(link, targets_parse)

1fea9e28674dc4ea2421cd6420a2a2edd93897b0
Konstantin <chachabooms@gmail.com>
2022-05-03 18:57:43 +0300

    fixed style

--- R/comments.R
+++ R/comments.R
@@ -84,8 +84,10 @@ num_links <- seq_len(nrow(links_parse))
 dt_load_now <- as.character(lubridate::now(tzone = time_local))
 all_comments <- purrr::map_df(num_links, \(x) get_comments(x)) |>
   dplyr::mutate(Rating = (.data$Rating / 100) * 5) |>
-  dplyr::mutate(Text = stringr::str_trim(.data$Text, side = "both"),
-                Author = stringr::str_trim(.data$Author, side = "both")) |>
+  dplyr::mutate(
+    Text = stringr::str_trim(.data$Text, side = "both"),
+    Author = stringr::str_trim(.data$Author, side = "both")
+  ) |>
   dplyr::mutate(Author = gsub(",$", "", .data$Author)) |>
   dplyr::mutate(dt_load = dt_load_now) |>
   dplyr::mutate(Date = format(lubridate::mdy(.data$Date), "%Y-%m-%d")) |>
